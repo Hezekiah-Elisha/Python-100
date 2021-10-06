58dc4ef421fc33f43628e18a0d61ee3aed8bb087
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2021-10-06 23:02:19 +0300

    #Day2: Middle letter

---
+++ mid.py
@@ -0,0 +1,8 @@
+def mid(name):
+    new_name = list(name)
+    if len(new_name) % 2 == 0:
+        return ""
+    else:
+        index = int(len(new_name) / 2)
+        return new_name[index]
+print(mid("hezekiaha"))
