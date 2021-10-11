d210f174fe54736210a30b9f0d4aeac83a83edd4
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2021-10-11 15:48:37 +0300

    #Day7: Adding and removing dots

---
+++ dots.py
@@ -0,0 +1,10 @@
+def add_dots(name):
+    return ".".join(name)
+
+def remove_dots(name):
+    return name.replace(".","")
+
+
+add_dots("hezekiah")
+remove_dots("h.e.z.e.k.i.a.h")
+print(remove_dots(add_dots("hezekiah")))
